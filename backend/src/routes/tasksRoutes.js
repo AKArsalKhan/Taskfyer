6c57fcffe350ffbc372e57ad904ca68d82568a9c
AKArsalKhan <arsal_khan12@hotmail.com>
2024-10-31 17:08:46 -0600

    backend changes

---
+++ backend/src/routes/tasksRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controllers/task/taskController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/task/create",protect,createTask);
+router.get("/tasks",protect,getTasks);
+router.get("/task/:id",protect,getTask);
+router.patch("/task/:id",protect,updateTask);
+router.patch("/task/:id",protect,deleteTask);   
+
+
+
+
+export default router;
